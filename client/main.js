7e6326d4f9a0fdfa59a8038024523940605f1e6e
varunrajasekhar <varunrajasekhar@gmail.com>
2018-04-24 15:58:12 -0500

    initial files commit

---
+++ client/main.js
@@ -0,0 +1,4 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { LoadAppComponent } from './js/components/load-app.component';
+
+bootstrap(LoadAppComponent);
---
+++ client/src/js/components/load-app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'load-app',
+    template: '<h1> Angular 4 </h1>'
+})
+export class LoadAppComponent { }
